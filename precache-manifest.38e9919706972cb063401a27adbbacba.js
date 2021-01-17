e0e5659898f937f2c78ef9299b5231bed8fc49b5
Metker <gustifaizalsani8@gmail.com>
2021-01-17 15:45:50 +0700

    Updates

---
+++ precache-manifest.38e9919706972cb063401a27adbbacba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff58eaa30c0cd814dd2c70dfde47cffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b0d68093d52fd7b5e07",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "877f11053ab304e17f8c",
+    "url": "/static/css/main.c42998c9.chunk.css"
+  },
+  {
+    "revision": "6b0d68093d52fd7b5e07",
+    "url": "/static/js/2.5952d5a0.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.5952d5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877f11053ab304e17f8c",
+    "url": "/static/js/main.af64c46f.chunk.js"
+  },
+  {
+    "revision": "8e9bda60e0a66ea0df9c",
+    "url": "/static/js/runtime-main.273dd2ce.js"
+  },
+  {
+    "revision": "cf694a6f82546772573b7dfc5c177942",
+    "url": "/static/media/hero.cf694a6f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/http://metkerr.github.io/precache-manifest.e7eb0702bc3851173264c1a51d93e9bc.js"
+  "/precache-manifest.38e9919706972cb063401a27adbbacba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/http://metkerr.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
