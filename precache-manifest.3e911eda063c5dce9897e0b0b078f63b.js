642baeca43efe60d5cfe356198e5da832c360946
Metker <64699075+metkerr@users.noreply.github.com>
2022-12-14 04:25:11 +0800

    clean up

---
+++ precache-manifest.3e911eda063c5dce9897e0b0b078f63b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cca6fa787aa2aabd2bcd3ad046f0b03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "261107592a237817c703",
+    "url": "/static/css/2.36eead04.chunk.css"
+  },
+  {
+    "revision": "df2134df0a015af727b4",
+    "url": "/static/css/main.73e46ab6.chunk.css"
+  },
+  {
+    "revision": "261107592a237817c703",
+    "url": "/static/js/2.e6a3e1ba.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e6a3e1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2134df0a015af727b4",
+    "url": "/static/js/main.b5b890f4.chunk.js"
+  },
+  {
+    "revision": "c941744023e0ff3697e9",
+    "url": "/static/js/runtime-main.239c1bc7.js"
+  },
+  {
+    "revision": "cf694a6f82546772573b7dfc5c177942",
+    "url": "/static/media/hero.cf694a6f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.776c405913c04510c9b8f7c7d9869979.js"
+  "/precache-manifest.3e911eda063c5dce9897e0b0b078f63b.js"
 );
 
 self.addEventListener('message', (event) => {
